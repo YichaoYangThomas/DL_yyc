e5276204ae0c681ac2612a8295bc28aabd234190
YichaoYangThomas <yy5020@nyu.edu>
2025-04-15 11:15:55 -0400

    '2'

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def get_device():
 
 
 def load_data(device):
-    data_path = "/scratch/DL24FA"
+    data_path = "/scratch/DL25SP"
 
     probe_train_ds = create_wall_dataloader(
         data_path=f"{data_path}/probe_normal/train",
